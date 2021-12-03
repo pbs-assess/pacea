5ee7df4cfddc3e54e8fa011a78a45927436184ad
Watson <Joe.Watson@dfo-mpo.gc.ca>
2021-12-03 10:54:20 -0800

    Added the first set of unit tests to the testthat folder.
    The first test script is test-Data_Key.R This runs a series of tests checking that the columns of Data_Key have been appropriately filled in and that all the data frames available in the data folder are included in the Data_Key.

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(PACea)
+
+test_check("PACea")
---
+++ tests/testthat/test-Data_Key.R
@@ -0,0 +1,49 @@
+context('Checking validity of Data_Key')
+
+library(stringr)
+
+setwd('./data')
+
+test_that('Are all Fetch_names unique in the Data_Key?',{
+  expect_equal(sum(duplicated(Data_Key$Fetch_Name)), 0)
+})
+
+test_that('Are all DF_Variable_Names unique in the Data_Key?',{
+  expect_equal(sum(duplicated(Data_Key$DF_Variable_Names)), 0)
+})
+
+test_that('Are all Time_Resolution Values valid in the Data_Key?',{
+  expect_equal(sum(!(Data_Key$Time_Resolution %in% c('Monthly','Annual','Fixed'))), 0)
+})
+
+test_that('Are any DF_Name values missing in the Data_Key?',{
+  expect_equal(sum(is.na(Data_Key$DF_Name)),0)
+})
+
+test_that('Are any Data_Type values missing in the Data_Key?',{
+  expect_equal(sum(is.na(Data_Key$Data_Type)),0)
+})
+
+test_that('Are any Units values missing in the Data_Key?',{
+  expect_equal(sum(is.na(Data_Key$Units)),0)
+})
+
+test_that('Are any Author values missing in the Data_Key?',{
+  expect_equal(sum(is.na(Data_Key$Author)),0)
+})
+
+test_that('Are any Citation values missing in the Data_Key?',{
+  expect_equal(sum(is.na(Data_Key$Citation)),0)
+})
+
+test_that('Have all data.frames present in the folder "/data" been added to the Data_Key?',{
+  expect_equal(length(unique(Data_Key$DF_Name)),
+               sum(grepl(list.files(), pattern='DF')))
+  if(length(unique(Data_Key$DF_Name)) != sum(grepl(list.files(), pattern='DF')) )
+  {
+    names_DF_files <- str_split(list.files(),pattern = '.rda', simplify = T)[,1]
+    names_DF_files <- names_DF_files[grepl(names_DF_files, pattern = 'DF')]
+    print(paste0('The following files appear to need adding to the Data_Key: ',
+                 names_DF_files[!(names_DF_files %in% Data_Key$DF_Name)]))
+  }
+})
