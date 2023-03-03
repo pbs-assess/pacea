3cab1bc1f63614e4dbeb0d94a1b657894b3604c8
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2023-03-02 21:03:31 -0800

    Figuring out making of grids, BC partition, and some of the ERDDAP code.

--- R/make-grid.R
+++ R/make-grid.R
@@ -1,14 +1,13 @@
 make_grid <- function(coastline,
-                      num_x = 10,
-                      num_y = 10,
-                      cell_size_x = 20,
-                      cell_size_y = 20,
+                      # num_x = 10,
+                      # num_y = 10,
+                      cell_size_x = 2,
+                      cell_size_y = 2,
                       rotang = 318.5){
 # DECIDE WHICH TO INCLUDE AS DEFAULT, num or cell size
 
-# create large pixel grid over the simplified coastline
+  # create large pixel grid over the simplified coastline
   # Rotate the grid to maximize the spatial overlap
-#  rotang = 318.5
 
   center <- sf::st_centroid(sf::st_union(coastline))
 
@@ -18,11 +17,11 @@ make_grid <- function(coastline,
   # intuitive, it should be each direction in km's.
 
   Coastline_Grid <- sf::st_sf(
-                        sf::st_make_grid(tran(sf::st_geometry(coastline),
-                                              -rotang,
-                                              center),
-                                         cellsize = c(cell_size_x,
-                                                      cell_size_y)))
+                          sf::st_make_grid(tran(sf::st_geometry(coastline),
+                                                -rotang,
+                                                center),
+                                           cellsize = c(cell_size_x,
+                                                        cell_size_y)))
 
 # Have this option for numbers of cells, though am thinking cell_size is more intuitive
 #  Coastline_Grid <- sf::st_sf(
@@ -34,6 +33,10 @@ make_grid <- function(coastline,
                          rotang,
                          center)
 
+  # May need the bit from Create_BC_Partition_New.Rmd that adds the extra
+  # 50km. Can do that in here, just the once, and then presumably save
+  # it. Waiting to see where Joe said he got Coastline.rda from.
+
   # Keep only the polygons that fall within or touch the original Coastline to save storage space
   Coastline_Grid <- sf::st_as_sf(sf::as_Spatial(Coastline_Grid))
   sf::st_crs(Coastline_Grid) <- sf::st_crs(coastline)
@@ -44,18 +47,23 @@ make_grid <- function(coastline,
   # The final grid cell is the top-right grid cell (n,n)
   # NEED TO CHANGE THE VALUES 10 TO MATCH THE CHOICE OF n=c(,) above!!
 # Only currently works for cell_size_x = cell_size_y = 20
-  Coastline_Grid$Poly_Name <- paste0('Grid_Index_(',
-                                     rep(1:17, each = 53), # repeat each value of 1:cols, nrows
-                                     ',',
-                                     rep(1:53, times = 17), # repeat sequence 1:nrows, ncols times
-                                     ')')
+#  Coastline_Grid$Poly_Name <- paste0('Grid_Index_(',
+#                                     rep(1:17, each = 53), # repeat each value of 1:cols, nrows
+#                                     ',',
+#                                     rep(1:53, times = 17), # repeat sequence 1:nrows, ncols times
+#                                    ')')
 # Don't get how that works given we've taken some out. BUT I don't think we want
 # to do the fancy x,y labelling. That will all be behind the scenes, and it's
 # just as simple to have a single number. Think someone suggested the x,y
 # format, probably thinking the user would see that more (also before we thought
 # about much higher resolution).
 
+  # This gets rid of the un-needed cells (AE: I don't understand how though)
   Coastline_Grid  <- Coastline_Grid[Coastline, ]
+
+  # Number each cell 1, 2, 3, ...
+  Coastline_Grid <- Coastline_Grid %>%
+    dplyr::mutate(id = row_number())
 
   Coastline_Grid
 }
