7e5cb41e257b1b1925099af6397b37466096331a
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2023-04-04 14:45:57 -0700

    Gets plot.pacea_t working, document() (that was the issue as it needs to add it to the NAMESPACE).

--- R/plot-pacea_t.R
+++ R/plot-pacea_t.R
@@ -10,30 +10,27 @@
 ##' \dontrun{
 ##'
 ##' }
-plot.paceat <- function(obj #,
-  #                       value = val,
-  #                       xlab = "Date",
-  #                       ylab = "Value",
-  #                      ...
+plot.pacea_t <- function(obj,
+                         value = val,
+                         xlab = "Date",
+                         ylab = "Value",
+                        ...
                          ){
   # TODO extract date-related columns and create the date column correctly
   #  This works for oni
 
-#  obj_lub <- dplyr::mutate(obj,
-#                           date = paste(year,
-#                                        month,
-#                                        sep = "-"))
-#  obj_lub$date <- lubridate::ym(obj_lub$date)
+  obj_lub <- dplyr::mutate(obj,
+                           date = paste(year,
+                                        month,
+                                        sep = "-"))
+  obj_lub$date <- lubridate::ym(obj_lub$date)
 
   # TODO could automate name for ylab, by adding it as an attribute (?) to each
   # object.
 
-#  plot.default(obj_lub$date,
-#               obj_lub$val,
-#               xlab = xlab,
-#               ylab = ylab,
-#              ...)
-
-  plot.default(obj$month,
-               col = "red") # to see if this function is being called
+  plot.default(obj_lub$date,
+               obj_lub$val,
+               xlab = xlab,
+               ylab = ylab,
+              ...)
 }
