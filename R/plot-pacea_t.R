5fb82253a9efb1294b94aa436c8d6070203e7a65
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2023-04-04 11:14:35 -0700

    Add plot-pacea_t.R to try and troubleshoot it.

---
+++ R/plot-pacea_t.R
@@ -0,0 +1,39 @@
+##' Plot a pacea time series object
+##'
+##'  <desc>
+##'
+##' @param obj a `pacea_t` object, which is a time series.
+##' @return
+##' @export
+##' @author Andrew Edwards
+##' @examples
+##' \dontrun{
+##'
+##' }
+plot.paceat <- function(obj #,
+  #                       value = val,
+  #                       xlab = "Date",
+  #                       ylab = "Value",
+  #                      ...
+                         ){
+  # TODO extract date-related columns and create the date column correctly
+  #  This works for oni
+
+#  obj_lub <- dplyr::mutate(obj,
+#                           date = paste(year,
+#                                        month,
+#                                        sep = "-"))
+#  obj_lub$date <- lubridate::ym(obj_lub$date)
+
+  # TODO could automate name for ylab, by adding it as an attribute (?) to each
+  # object.
+
+#  plot.default(obj_lub$date,
+#               obj_lub$val,
+#               xlab = xlab,
+#               ylab = ylab,
+#              ...)
+
+  plot.default(obj$month,
+               col = "red") # to see if this function is being called
+}
