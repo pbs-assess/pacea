0f149fc24c06c59f479a42814a0c1f3d9397735f
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2023-04-14 17:07:19 -0700

    Tweak documentation.

--- R/pacea-cache.R
+++ R/pacea-cache.R
@@ -12,10 +12,10 @@
 ##'
 ##' @return the cache directory on the user's machine, based on
 ##'   `rappdirs::user_cache_dir("pacea")`. On Windows in 2023 this seems to be
-##'   `C:\\Users\\<username>\\AppData\\Local\pacea\Cache`. If you want to
+##'   `C:\\Users\\<username>\\AppData\\Local\\pacea\\Cache`. If you want to
 ##'   put use something else then see `?rappdirs::user_cache_dir` and change
 ##'   your `R_USER_CACHE_DIR`. To do that see
-##'   \href{https://github.com/r-lib/rappdirs/issues/34}.
+##'   `https://github.com/r-lib/rappdirs/issues/34`.
 ##'
 ##' @export
 ##' @author Andrew Edwards
