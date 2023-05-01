97cf8aedb2eb8eb5ba957b52270fa045360874ba
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2023-05-01 16:39:50 -0700

    Add hake spawning biomass data object calculation.

--- data-raw/groundfish/hake.R
+++ data-raw/groundfish/hake.R
@@ -3,11 +3,13 @@
 
 load_all()
 
-# Get this file from Andy running
+# Get the .rda files from Andy running
 #  hake-assessment/sandbox/andy/pacea-save/pacea-save.R
 #  after 2023 hake assessment and copying it here.
 #  For future assessments will maybe convert that to
 #  a function. Check with Andy or Chris Grandin.
+
+# Recruitment
 
 load("hake_recruitment_new.rda")                #
 
@@ -23,4 +25,22 @@ if(check_index_changed(hake_recruitment,
   usethis::use_data(hake_recruitment,
                     overwrite = TRUE)
   plot(hake_recruitment, value = "val", style = "plain")
+}
+
+# Spawning biomass
+
+load("hake_biomass_new.rda")                #
+
+class(hake_biomass_new) <- c("pacea_t",
+                             class(hake_biomass_new))
+
+attr(hake_biomass_new, "axis_name") <-
+  "Pacific Hake spawning biomass (million t)"
+
+if(check_index_changed(hake_biomass,
+                       hake_biomass_new)){
+  hake_biomass <- hake_biomass_new
+  usethis::use_data(hake_biomass,
+                    overwrite = TRUE)
+  plot(hake_biomass, value = "val", style = "plain")
 }
