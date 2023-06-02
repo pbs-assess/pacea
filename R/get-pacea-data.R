cf66dce073be4a266d21ee078c0121be1c9d34e9
Travis Tai <ttai2@alumni.uwo.ca>
2023-06-02 13:12:52 -0700

    Added update data option, with interactive options

--- R/get-pacea-data.R
+++ R/get-pacea-data.R
@@ -5,6 +5,7 @@
 #' Some large pacea data sets are stored in a GitHub repository, pacea-data, and get_pacea_data allows users to choose which data to download locally. Refer to paceadata_list(?EDIT?) to view the data files available for download. Code adapted from 'bcmaps'
 #'
 #' @param layer Name of the data object.
+#' @param update Logical. Would you like to check for a newer version of the layer? 
 #' @param ask Logical. Should the user be asked before downloading the data to local cache? Defaults to the value of interactive().
 #'
 #' @return Data object requested
@@ -19,7 +20,7 @@
 #' }
 #' 
 
-get_pacea_data <- function(layer, ask = interactive()) {
+get_pacea_data <- function(layer, update = FALSE, ask = interactive()) {
   
   ## edit message
   if (!is.character(layer)) {
@@ -42,14 +43,106 @@ get_pacea_data <- function(layer, ask = interactive()) {
   # if file already exists
   if (length(grep_list) > 0) {
     
-    filename <- grep_list[order(grep_list, decreasing = T)][1]
+    local_filename <- grep_list[order(grep_list, decreasing = T)][1]
     
-    file_dir <- paste0(cache_dir, "/", filename)
+    local_file_dir <- paste0(cache_dir, "/", local_filename)
     
-    dat <- readRDS(file_dir)
-    return(dat)
+    if (update) {
+      
+      ## internet errors for downloading
+      if (!checkInternetConnection()) {
+        
+        warning("No access to internet - could not check for updates.", call. = FALSE) 
+        
+        dat <- readRDS(local_file_dir)
+        return(dat)
+        
+      }
+      
+      fileurl <- paste0("https://github.com/pbs-assess/pacea-data/blob/main/data/", layer, ".rds?raw=true") 
+      
+      # Send GET request to retrieve the file
+      response <- httr::GET(fileurl)
+      
+      # Check if the request was successful
+      if (httr::status_code(response) == 200) {
+        
+        # Read the content of the response as raw data
+        content_raw <- httr::content(response, "raw")
+        
+        # Create a temporary file path
+        temp_file <- tempfile()
+        
+        # Write the raw data to the temporary file
+        writeBin(content_raw, temp_file)
+        
+        # Read the .rds file into R and get name of file (can be different than path name)
+        dat_name <- load(temp_file)
+        
+        # name data to generic object name
+        dat <- get(dat_name)
+        
+        # Remove the temporary file
+        file.remove(temp_file) 
+        
+        # compare versions of local file vs internet file
+        tlocal_filename <- substr(local_filename, 1, nchar(local_filename) - 4)
+        local_file_version <- substr(tlocal_filename, nchar(tlocal_filename) - 1, nchar(tlocal_filename))
+        web_file_version <- substr(dat_name, nchar(dat_name) - 1, nchar(dat_name))
+        
+        if(local_file_version == web_file_version){
+          
+          message("Most recent version of data already downloaded in cache folder!")
+          
+          dat <- readRDS(local_file_dir)
+          return(dat)
+          
+        } else {
+          
+          if (ask) {
+            ans <- ask(paste("Newer version of data available and will delete previous version in local cache folder:",
+                             cache_dir, "Is that okay?", sep = "\n"))
+            if (!ans) {
+              
+              message("Returned local version of data.")
+              
+              dat <- readRDS(local_file_dir)
+              return(dat)
+              
+            } else {
+              
+              # create file name with version number
+              filename <- paste0(dat_name,".rds")
+              file_dir <- paste0(cache_dir, "/", filename)
+              
+              saveRDS(dat, file = file_dir, compress = "xz")
+              
+              # delete previous version in local folder
+              unlink(local_file_dir)
+              
+              return(dat)
+              
+              message("Data successfully updated!")
+              
+            }
+          } 
+        }
+        
+      } else { 
+        
+        dat <- readRDS(local_file_dir)
+        return(dat)
+        
+        warning("Failed to download an updated version of the file.")
+      }
+      
+    } else { # no update
+      
+      dat <- readRDS(local_file_dir)
+      return(dat)
+    }
     
-  } else {
+  } else { # if file doesn't exist
     
     ## internet errors for downloading
     if (!checkInternetConnection()) stop("No access to internet", call. = FALSE) 
@@ -72,10 +165,6 @@ get_pacea_data <- function(layer, ask = interactive()) {
     
     fileurl <- paste0("https://github.com/pbs-assess/pacea-data/blob/main/data/", layer, ".rds?raw=true") 
     
-    filename <- paste0(layer,".rds")
-    
-    file_dir <- paste0(cache_dir, "/", filename)
-    
     # Send GET request to retrieve the file
     response <- httr::GET(fileurl)
     
@@ -90,11 +179,19 @@ get_pacea_data <- function(layer, ask = interactive()) {
       # Write the raw data to the temporary file
       writeBin(content_raw, temp_file)
       
-      # Read the .rds file into R
-      dat <- get(load(temp_file))
+      # Read the .rds file into R and get name of file (can be different than path name)
+      #dat <- get(load(temp_file))
+      dat_name <- load(temp_file)
+      
+      # name data to generic object name
+      dat <- get(dat_name)
       
       # Remove the temporary file
-      file.remove(temp_file) 
+      file.remove(temp_file)
+      
+      # create file name with version number
+      filename <- paste0(dat_name,".rds")
+      file_dir <- paste0(cache_dir, "/", filename)
       
       saveRDS(dat, file = file_dir, compress = "xz")
 
@@ -105,12 +202,11 @@ get_pacea_data <- function(layer, ask = interactive()) {
       stop("Error: Failed to download the file.")
       
     }
-    
   }
 }
 
 
-#' Interactive function for Yes/No question
+#' Interactive function for Yes/No question - from bcmaps package
 #' @noRd
 ask <- function(...) {
   choices <- c("Yes", "No")
@@ -122,17 +218,7 @@ ask <- function(...) {
 #' @noRd
 checkInternetConnection <- function() {
   url <- "http://www.google.com"
-  con <- tryCatch({
-    url("http://www.google.com", timeout = 5)
-  }, error = function(e) {
-    return(FALSE)
-  })
-  
-  if (inherits(con, "try-error")) {
-    return(FALSE)
-  } else {
-    return(TRUE)
-  }
+  response <- try(httr::GET(url), silent=TRUE)
+  status_code <- try(status_code(response), silent=TRUE)
+  return(status_code == 200)
 }
-
-
