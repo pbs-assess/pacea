6183fefd7dd29dd9027b3bbdafecfdaa1f95c532
Travis Tai <ttai2@alumni.uwo.ca>
2023-06-12 10:35:50 -0700

    Testing for point2rast function in interpolate.

--- tests/testthat/test-interpolate.R
+++ tests/testthat/test-interpolate.R
@@ -21,5 +21,65 @@ test_that("Errors work", {
   # loc must be 2 col; loc length must of same length as data
   expect_error(point2rast(dat2, spatobj = extent, loc = ll1, cellsize = 0.5))
   expect_error(point2rast(dat2, spatobj = extent, loc = ll2, cellsize = 0.5))
-  
 })
+
+
+test_that("Warnings for loc coordinates outside extent of object to interpolate to.", {
+  dat <- data.frame(x = runif(5, 10, 20), y = runif(5, 10, 20), var = rnorm(5))
+  
+  extent <- st_bbox(c(xmin = 0, ymin = 0, xmax= 10, ymax = 10), crs = NA)
+  
+  expect_warning(point2rast(dat, extent, loc = c("x","y"), cellsize = 0.5, nnmax = 2, as = "SpatRast"))
+})
+
+
+
+test_that("Output of SpatialRaster and SpatialVector from dataframe successful.", {
+  dat <- data.frame(x = runif(5, 0, 10), y = runif(5, 0, 10), var = rnorm(5))
+  
+  extent <- st_bbox(c(xmin = 0, ymin = 0, xmax= 10, ymax = 10), crs = NA)
+  
+  output.rast <- point2rast(dat, extent, loc = c("x","y"), cellsize = 0.5, nnmax = 2, as = "SpatRast")
+  output.vect <- point2rast(dat, extent, loc = c("x","y"), cellsize = 0.5, nnmax = 2, as = "SpatVect")
+  
+  expect_equal(class(output.rast)[1], "SpatRaster")
+  expect_equal(nrow(output.rast), 20)
+  
+  expect_equal(class(output.vect)[1], "SpatVector")
+  expect_equal(nrow(output.vect), 400)
+})
+
+
+test_that("Output from sf object successful", {
+  dat <- data.frame(x = runif(5, 0, 10), y = runif(5, 0, 10), var = rnorm(5)) %>% 
+    st_as_sf(coords = c("x", "y"))
+  
+  extent <- st_bbox(c(xmin = 0, ymin = 0, xmax= 10, ymax = 10), crs = NA)
+  
+  output.rast <- point2rast(dat, extent, cellsize = 0.5, nnmax = 2, as = "SpatRast")
+  
+  expect_equal(class(output.rast)[1], "SpatRaster")
+  expect_equal(nrow(output.rast), 20)
+})
+
+
+
+test_that("Output from Spatial object successful", {
+  dat <- data.frame(x = runif(5, 0, 10), y = runif(5, 0, 10), var = rnorm(5)) %>% 
+    st_as_sf(coords = c("x", "y")) %>% as_Spatial()
+    
+  extent <- st_bbox(c(xmin = 0, ymin = 0, xmax= 10, ymax = 10), crs = NA)
+  
+  output.rast <- point2rast(dat, extent, cellsize = 0.5, nnmax = 2, as = "SpatRast")
+  
+  expect_equal(class(output.rast)[1], "SpatRaster")
+  expect_equal(nrow(output.rast), 20)
+})
+
+
+
+
+
+
+
+
