a1473626b92849887d3cc637feacf1f5e83134f3
Travis Tai <ttai2@alumni.uwo.ca>
2023-07-28 05:54:05 -0700

    Update data-list.

--- data-raw/data-key/data-list.R
+++ data-raw/data-key/data-list.R
@@ -1,7 +1,8 @@
 # generate pacea_data data object for list of files
 
-pacea_data <- read.csv("./data-raw/data-key/data_list.csv")
+pacea_data <- read.csv("./data-raw/data-key/data_list.csv", header = T)
+pacea_data <- pacea_data[order(pacea_data$data_name),]
 
-use_data(pacea_data, overwrite = T)
+use_data(pacea_data, overwrite = TRUE)
 
 
