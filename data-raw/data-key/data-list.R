4c4fefbc7675339f0aab4a2285284347ae4f6671
Travis Tai <ttai2@alumni.uwo.ca>
2023-08-22 07:27:53 -0700

    Rename roms_data list.

--- data-raw/data-key/data-list.R
+++ data-raw/data-key/data-list.R
@@ -1,7 +1,7 @@
 # generate pacea_data data object for list of files
 library(dplyr)
 
-pacea_data <- read.csv("./data-raw/data-key/data_list.csv", header = T) %>% 
+roms_data <- read.csv("./data-raw/data-key/data_list.csv", header = T) %>% 
   arrange(data_name)
 
-use_data(pacea_data, overwrite = TRUE)
+use_data(roms_data, overwrite = TRUE)
