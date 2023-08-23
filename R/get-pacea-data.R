785ee56c5621e7e73ac2f09bc229a408a2585817
Travis Tai <ttai2@alumni.uwo.ca>
2023-08-23 07:03:04 -0700

    roms_data name change.

--- R/get-pacea-data.R
+++ R/get-pacea-data.R
@@ -33,7 +33,7 @@ get_pacea_data <- function(layer, update = FALSE, ask = interactive(), force = F
   test_names <- c("test_data", "test_data_01", "test_data_02", "test_corruptdata")
   
   ## find data in row - CHANGE datalist name if necessary
-  data_list <- pacea_data
+  data_list <- roms_data
   data_row <- data_list[grep(layer, data_list[["data_name"]], ignore.case = TRUE), , drop = FALSE]  
   if (nrow(data_row) != 1L && !(layer %in% test_names)) {
     stop(layer, " is not an available data object")
