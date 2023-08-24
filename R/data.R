e06d9e13bc5cd06691386774b3f05eaa51501c7c
Travis Tai <ttai2@alumni.uwo.ca>
2023-08-24 15:13:31 -0700

    Update data documentation.

--- R/data.R
+++ R/data.R
@@ -894,23 +894,33 @@
 
 #' Optimal interpolation SST
 #' 
+#' @description 
 #' Simple features objects of NOAA's Optimal Interpolation SST
-#'
+#' \describe{
+#'   \item{oisst_7day}{7-day mean sea surface temperature (deg C)}
+#'   \item{oisst_7dayclim}{7-day mean climatology (1991-2020) for sea surface temperature (deg C)}
+#'   \item{oisst_month}{Monthly mean sea surface temperature in (deg C}
+#'   \item{oisst_monthclim}{Monthly climatology (1991-2020) for sea surface temperature (deg C)}
+#' }
+#' 
 #' @details 
 #' From NOAA:
 #' "The NOAA 1/4° Daily Optimum Interpolation Sea Surface Temperature (OISST) is a long term Climate Data Record that incorporates observations from different platforms (satellites, ships, buoys and Argo floats) into a regular global grid. The dataset is interpolated to fill gaps on the grid and create a spatially complete map of sea surface temperature. Satellite and ship observations are referenced to buoys to compensate for platform differences and sensor biases."
 #'
 #' The data starts from 1981-09-01 and ends in the previous month from the current month. These data will be updated monthly. 
 #' 
+#' @format 
 #' \describe{
-#'   \item{oisst_7day}{7-day mean sea surface temperature (degrees Celsius)}
-#'   \item{oisst_7dayclim}{7-day mean climatology (1991-2020) for sea surface temperature (degrees Celsius)}
-#'   \item{oisst_month}{Monthly mean sea surface temperature in degrees Celsius}
-#'   \item{oisst_monthclim}{Monthly climatology (1991-2020) for sea surface temperature (degrees Celsius)}
+#'   \item{year}{Year}
+#'   \item{week}{Calendar week}
+#'   \item{month}{Calendar month}
+#'   \item{sst}{Mean sea surface temperature, SST (deg C)}
+#'   \item{sst_sd}{Standard deviation of SST}
+#'   \item{sst_n}{Sample size of SST}
+#'   \item{start_date}{Start date of mean SST estimate}
+#'   \item{end_date}{End date of mean SST estimate}
 #' }
-#' 
-#' @format `oisst_7day`
-#' A object of class sf with 2051650 rows and 8 columns.
+#' `oisst_7day` An object of class sf with 2051650 rows and 8 columns.
 #' 
 #' @usage oisst_7day
 #' @examples
@@ -920,23 +930,24 @@
 #' }
 #'
 #' @source https://www.ncei.noaa.gov/products/optimum-interpolation-sst
+#' 
 #' Generated from running `data-raw/erddap/OISST.R`.
 "oisst_7day"
 
 #' @rdname oisst_7day
-#' @format `oisst_7dayclim`
-#' An object of class sf with 49025 rows and 6 columns.
+#' @format 
+#' `oisst_7dayclim` An object of class sf with 49025 rows and 6 columns.
 #' @usage oisst_7dayclim
 "oisst_7dayclim"
 
 #' @rdname oisst_7day
-#' @format `oisst_month`
-#' An object of class sf with 464350 rows and 8 columns.
+#' @format 
+#' `oisst_month` An object of class sf with 464350 rows and 8 columns.
 #' @usage oisst_month
 "oisst_month"
 
 #' @rdname oisst_7day
-#' @format `oisst_monthclim`
-#' An object of class sf with 11100 rows and 6 columns.
+#' @format 
+#' `oisst_monthclim` An object of class sf with 11100 rows and 6 columns.
 #' @usage oisst_monthclim
 "oisst_monthclim"
