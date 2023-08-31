d0aeb8f848712f5cecb826a85f04c92e2f347259
Travis Tai <ttai2@alumni.uwo.ca>
2023-08-31 14:29:24 -0700

    bccm_all_variables download init commit. Update documentation.

--- R/shortcuts-get-data.R
+++ R/shortcuts-get-data.R
@@ -1,9 +1,12 @@
 #' BCCM environmental variable data 
 #' 
 #' @description 
-#' British Columbia continental margin (BCCM) model data for the BC Pacific region.
+#' Loading and/or downloading British Columbia continental margin (BCCM) model data for the BC Pacific region to local drive.
 #' 
 #' @details
+#' 
+#' The following functions serve to download specific individual ocean variables of the BCCM model data. Use `bccm_all_variables()` if you would like to download all variables - see help file `?bccm_all_variables` for details. If a variable has already been downloaded, the function will load data from `pacea_cache()` folder into user's local R environment.
+#' 
 #' The British Columbia continental margin (BCCM) model is an ocean circulation-biogeochemical model implementation of the regional ocean modelling system (ROMS). It has a horizontal resolution of 3km x 3km and a vertical discretization based on bathymetry of 42 depth levels increasing in resolution near the surface. These modelled output data were provided by Angelica Pena and the data is further detailed in Pena et al. (2019). 
 #' 
 #' BCCM data were interpolated to a 2km x 2km inshore grid and a 6km x 6km offshore grid. Data provided are monthly means that span from 1993-2019. Each column represents a distinct 'year_month' combination (e.g. 2010_4).
@@ -172,5 +175,43 @@ bccm_phytoplankton <- function(update = FALSE, ask = interactive(), force = FALS
 #' @export
 bccm_primaryproduction <- function(update = FALSE, ask = interactive(), force = FALSE) {
   get_pacea_data("bccm_primaryproduction", update = update, ask = ask, force = force)
+}
+
+
+
+#' Download all BCCM data
+#' 
+#' This function downloads all BCCM model data. 
+#' 
+#' @details 
+#' 
+#' Use `bccm_data` To view the full list of available variables
+#' 
+#' CAUTION: The BCCM files are large and downloading all data may take a while.
+#'
+#' @return downloaded files to `pacea_cache()` directory
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' bccm_all_variables()
+#' }
+bccm_all_variables <- function() {
+  
+  cache_dir <- pacea_cache()
+  
+  ans <- ask(paste("Downloading all BCCM data may take many minutes. Files will be downloaded to pacea_cache directory:",
+                   cache_dir, "Would you like to continue?", sep = "\n"))
+  
+  if (!ans) stop("Exiting...", call. = FALSE)
+  
+  bccm.datalist <- bccm_data
+  
+  for(i in 1:nrow(bccm.datalist)){
+    data.name <- bccm.datalist[i, 1]
+    get_pacea_data(data.name, force = TRUE)
+  }
+  
+  return(print("Download of all BCCM files: successful!"))
 }
 
