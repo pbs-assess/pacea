3fea7075b49a6ca685f05ba321278f20faed8804
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2023-10-23 12:07:24 -0700

    Re-order bccm variable names to keep more logical, which updates bccm_data, remove Joe's old data key; #41.

--- data-raw/data-key/data-list.R
+++ data-raw/data-key/data-list.R
@@ -1,7 +1,9 @@
 # generate pacea_data data object for list of files
 library(dplyr)
 
-bccm_data <- read.csv("./data-raw/data-key/data_list.csv", header = T) %>% 
-  arrange(data_name)
+bccm_data <- read.csv(paste0(here::here(),
+                             "/data-raw/data-key/data_list.csv"),
+                      header = TRUE)
 
-use_data(bccm_data, overwrite = TRUE)
+use_data(bccm_data,
+         overwrite = TRUE)
