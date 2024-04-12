5d37e7cc9afc2492824ca0c764ff86b6ae6b6e67
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-04-12 13:55:31 -0700

    Update and improve the zooplankton objects and plot.pacea_zooplankton(), document().

--- R/plot-pacea-zooplankton.R
+++ R/plot-pacea-zooplankton.R
@@ -1,23 +1,29 @@
 ##' Plot the one of the zooplankton anomalies.
 ##'
 ##' Temporal plot for one of the columns of a pacea zooplankton
-##' (`pacea_zooplankton`) object, as defined in `?zooplankton`. The specified
+##' (`pacea_zooplankton`) object, as defined in `?zooplankton_sog`. The specified
 ##' `species_group` gets passed onto `plot.pacea_index()` as the anomaly to be
-##' plotted, as do any options specified here for display style, adding in
-##' times of other events, etc. See examples and vignette, as well as the
+##' plotted, and yields a correctly automated y-axis label (that can be user-specified if
+##' desired). Other options specified here also get passed onto
+##' `plot.pacea_index()` and `plot()`, such as for display style, adding in
+##' times of other events, etc. See examples and zooplankton vignette, as well as the
 ##' climatic and oceanographic indices vignette.
 ##'
 ##' @param obj a `pacea_zooplankton` object, which is a tibble with columns
 ##'   giving `year`, `num_samples` (number of samples), `volume_filtered`,
-##'   `total_biomass`, and then 24 species groups of zooplankton (see `?zooplankton`).
+##'   `total_biomass`, and then 24 species groups of zooplankton (see
+##'   `?zooplankton_sog`, currently the only zooplankton data in pacea).
 ##' @param species_group which column of `obj` to plot, either `total_biomass`
-##'   (the default) or one of the 24 species groups described in `?zooplankton`.
+##'   (the default) or one of the 24 species groups described in `?zooplankton_sog`.
 ##' @param xlab x-axis label, defaults to `Date`
-##' @param ylab y-axis label, if `NULL` (the default` then automatically selects
+##' @param ylab y-axis label, if `NULL` (the default) then automatically selects
 ##'   the `axis_name` corresponding to `species_group` in the object
-##'   `zooplankton_axis_names`. User can also just provide their own label.
+##'   `zooplankton_sog_axis_names`. User can also just provide their own label.
 ##' @param lwd line thickness to plot the anomalies, defaults to 15 since these
-##'   are annual; likely needs tweaking depending on the size of the final figure.
+##'   are annual; likely needs tweaking depending on the size of the final
+##'   figure.
+##' @param mgp_val mgp value for `par()`, first value is slightly less than the
+##'   default to fit the superscripts in okay. Resets to existing value after plotting.
 ##' @param ... optional arguments passed onto `plot.pacea_index()` and
 ##'   `plot()`. Note that the x-axis is constructed using a lubridate `date`
 ##'   object, so `xlim` needs to be a  `date` object (see `plot.pacea_index()` example).
@@ -27,33 +33,30 @@
 ##' @author Andrew Edwards
 ##' @examples
 ##' \dontrun{
-##' plot(oni)
-##' plot(oni,
-##'      xlim = c(lubridate::dmy(01011950),
-##'      lubridate::dmy(01012040))) # to expand x-axis
-##' plot(npi_monthly,
-##'      value = "value")
-##' plot(oni,
-##'      event_years = bluntnose_caught_years,
-##'      xlim = c(lubridate::dmy(01011995), lubridate::dmy(01012024)),
-##'      lwd = 2)
+##' # Also see zooplankton vignette
+##' plot(zooplankton_sog)    # Default is total_biomass, calls plot.pacea_zooplankton()
+##' plot(zooplankton_sog, species_group = "cladocera")  # y-axis name is automated
 ##' }
 plot.pacea_zooplankton <- function(obj,
                              species_group = "total_biomass",
                              xlab = "Date",
                              ylab = NULL,
                              lwd = 15,
+                             mgp_val = c(2.5, 1, 0),
                              ...
                              ){
 
   stopifnot("species_group must be a column of the zooplankton object" =
               species_group %in% names(obj))
 
+  mgp_orig <- par()$mgp
+  par(mgp = mgp_val)
+
   obj_to_plot <- dplyr::select(obj,
                                year,
                                anomaly = eval(species_group))
   if(is.null(ylab)){
-    ylab <- filter(zooplankton_axis_names,
+    ylab <- filter(zooplankton_sog_axis_names,
                    species_group_name == species_group)$axis_name
   }
 
@@ -61,4 +64,7 @@ plot.pacea_zooplankton <- function(obj,
                    ylab = ylab,
                    lwd = lwd,
                    ...)
+
+  par(mgp = mgp_orig)  # Leave things as you found them
+  invisible()
 }
--- data-raw/zooplankton/zooplankton.R
+++ data-raw/zooplankton/zooplankton.R
@@ -14,6 +14,7 @@
 load_all()
 library(dplyr)
 
+# Change this when get updated data
 raw_orig <-
   readr::read_csv("2024/2024_Feb-zoop df_anomalies_deepSoG_forRpacea_1996-2021baseline.csv") %>%
   dplyr::mutate_if(is.character, factor)
@@ -23,42 +24,42 @@ raw_orig <-
 # consistency. Can give full definitions in the help file.
 
 # If re-order these at all (there should not be a reason, these are all alphabetical), then have to re-order zooplankton_axis_names below
-zooplankton_new <- select(raw_orig,
-                          year,
-                          number_samples = n.samples,
-                          volume_filtered = vol.filt,
-                          total_biomass = TotalBiomass,
-                          amphipods_gammarid = AmphiGam,
-                          amphipods_hyperiid = AmphiHyp,
-                          benthic_larvae = BenthicLarv,
-                          calanoid_copepods_large = CalCops.larg,
-                          calanoid_copepods_medium = CalCops.med,
-                          calanoid_copepods_small = CalCops.smal,
-                          cephalopoda = Cephalopoda,
-                          chaetognatha = Chaetognatha,
-                          cladocera = Cladocera,
-                          ctenophora = Ctenophora,
-                          euphausiids = Euphs,
-                          fish = Fish,
-                          larvacea = Larvacea,
-                          medusae = Medusae,
-                          mysids = Mysids,
-                          natantia = Natantia,
-                          non_calanoid_copeopods = NonCalCops,
-                          ostracoda = Ostracoda,
-                          other = Other,
-                          pelagic_polychaeta = PolychaetPelagic,
-                          pteropods = Pteropods,
-                          repantia = Repantia,
-                          scyphozoa = Scyphozoa,
-                          siphonophorae = Siphonophorae)
+zooplankton_sog_new <- select(raw_orig,
+                              year,
+                              number_samples = n.samples,
+                              volume_filtered = vol.filt,
+                              total_biomass = TotalBiomass,
+                              amphipods_gammarid = AmphiGam,
+                              amphipods_hyperiid = AmphiHyp,
+                              benthic_larvae = BenthicLarv,
+                              calanoid_copepods_large = CalCops.larg,
+                              calanoid_copepods_medium = CalCops.med,
+                              calanoid_copepods_small = CalCops.smal,
+                              cephalopoda = Cephalopoda,
+                              chaetognatha = Chaetognatha,
+                              cladocera = Cladocera,
+                              ctenophora = Ctenophora,
+                              euphausiids = Euphs,
+                              fish = Fish,
+                              larvacea = Larvacea,
+                              medusae = Medusae,
+                              mysids = Mysids,
+                              natantia = Natantia,
+                              non_calanoid_copeopods = NonCalCops,
+                              ostracoda = Ostracoda,
+                              other = Other,
+                              pelagic_polychaeta = PolychaetPelagic,
+                              pteropods = Pteropods,
+                              repantia = Repantia,
+                              scyphozoa = Scyphozoa,
+                              siphonophorae = Siphonophorae)
 
-class(zooplankton_new) <- c("pacea_zooplankton",
-                            class(zooplankton_new))
+class(zooplankton_sog_new) <- c("pacea_zooplankton",
+                                class(zooplankton_sog_new))
 
-plot(zooplankton_new)
-tail(zooplankton_new)
-tail(zooplankton)     # what's currently in pacea. Will know if we're updating
+plot(zooplankton_sog_new)
+tail(zooplankton_sog_new)
+tail(zooplankton_sog)     # what's currently in pacea. Will know if we're updating
                       # data, so no need to check automatically like for oni etc.
 
 # for 2025 update, do a check to see whether historic values have changed, and
@@ -67,46 +68,47 @@ tail(zooplankton)     # what's currently in pacea. Will know if we're updating
 
 # Check that axis names are still consistent; if fail then manually change
 #  zooplankton_axis_names definition below
-expect_equal(names(zooplankton_new),
+expect_equal(names(zooplankton_sog_new),
              c("year",
                "number_samples",
                "volume_filtered",
-               pull(zooplankton_axis_names,
-                    name)))
+               pull(zooplankton_sog_axis_names,
+                    species_group_name)))
 
 # Axis names, based on 2024/2024_biomass_definitions.csv.
-zooplankton_axis_names_new <- tibble(
-  species_group_name = names(zooplankton_new)[-c(1, 2, 3)],
-  axis_name = c("Total biomass",
-                "Gammarid amphipods",
-                "Hyperiid amphipods",
-                "Benthic larvae",
-                "Calanoid copepods (large)",
-                "Calanoid copepods (medium)",
-                "Calanoid copepods (small)",
-                "Cephalopoda",
-                "Chaetognatha",
-                "Cladocera",
-                "Ctenophora",
-                "Euphausiids",
-                "Larval fish",
-                "Larvacea",
-                "Medusae",
-                "Mysids",
-                "Natantia",
-                "Non-calanoid copeopods",
-                "Ostracoda",
-                "Other taxa",
-                "Pelagic polychaeta",
-                "Pteropods",
-                "Repantia",
-                "Scyphozoa",
-                "Siphonophorae"))
+#  Adapting methods for names used in ALPI.
+zooplankton_sog_axis_names_new <- tibble(
+  species_group_name = names(zooplankton_sog_new)[-c(1, 2, 3)],
+  axis_name = c(expression(paste(plain(Total) * " " * plain(biomass) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Gammarid) * " " * plain(amphipods) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Hyperiid) * " " * plain(amphipods) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Benthic) * " " * plain(larvae) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Calanoid) * " " * plain(copepods) * " " * plain(large) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Calanoid) * " " * plain(copepods) * " " * "(" * plain(medium) * ")" * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Calanoid) * " " * plain(copepods) * " " * "(" * plain(small) * ")" * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Cephalopoda) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Chaetognatha) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Cladocera) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Ctenophora) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Euphausiids) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Larval) * " " *  plain(fish) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Larvacea) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Medusae) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Mysids) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Natantia) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Non-calanoid) * " " *  plain(copeopods) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Ostracoda) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Other) * " " *  plain(taxa) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Pelagic) * " " *  plain(polychaeta) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Pteropods) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Repantia) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Scyphozoa) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2)),
+                expression(paste(plain(Siphonophorae) * " " * plain(anomaly) * ", " * log[10] * " " * g * " " * m^-2))))
 
-zooplankton <- zooplankton_new
-usethis::use_data(zooplankton,
+zooplankton_sog <- zooplankton_sog_new
+usethis::use_data(zooplankton_sog,
                   overwrite = TRUE)
 
-zooplankton_axis_names <- zooplankton_axis_names_new
-usethis::use_data(zooplankton_axis_names,
+zooplankton_sog_axis_names <- zooplankton_sog_axis_names_new
+usethis::use_data(zooplankton_sog_axis_names,
                   overwrite = TRUE)
