644ecbe3afa9a2e5af997640391e8af434a36eb3
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-04-25 15:31:27 -0700

    One more nocov, in clear-cache.R, which I had being skipped earlier through exclusions; coverage should be 100%.

--- R/clear-cache.R
+++ R/clear-cache.R
@@ -2,9 +2,9 @@
 #' pacea clear cache
 #'
 #' Delete all data files in cache folder.
-#' 
+#'
 #' This function will prompt the user on whether to delete all files in the cache directory. This cannot be undone and the user will have to download the files again.
-#' 
+#'
 #' @export
 #'
 #' @examples
@@ -13,29 +13,28 @@
 #' list.files(pacea_cache())
 #' pacea_clear_cache()
 #' }
-#' 
+#'
 pacea_clear_cache <- function() {
-  
+
   cache_dir <- pacea::pacea_cache()
-  
+
   cache_list <- list.files(pacea_cache())
-  
+
   ## interactive ask to delete files from cache
   ans <- ask(paste("About to delete ALL data files in cache directory:",
                    cache_dir, "Is that okay?", sep = "\n"))
-  
-  if (!ans) stop("Exiting...", call. = FALSE)
-  
+
+  if (!ans) stop("Exiting...", call. = FALSE) # nocov
+
   pat <- "/bccm_*"
-  
+
   # calling scope - testthat detection
   tb <- .traceback(x = 0)
   if(any(unlist(lapply(tb, function(x) any(grepl("test_env", x)))))){
     pat <- "/test_*"
   }
-  
+
   unlink(paste0(cache_dir, pat))
-  
+
   cat("All BCCM data files removed from cache.")
 }
-
