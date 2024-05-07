aebe72b61e2b3457272d8009b1d7e5f632cd38fd
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-05-07 16:18:30 -0700

    Rename test file.

--- tests/testthat/test-create-data.R
+++ tests/testthat/test-create-data.R
@@ -1,9 +1,9 @@
-test_that("create_data_hake() works", {
+test_that("create_data() works", {
   x <- 1:10
   year <- 2048
-  expect_invisible(create_data_hake(paste0("test_file_",
-                                           year),
-                                    x))
+  expect_invisible(create_data(paste0("test_file_",
+                                      year),
+                               x))
   file.remove(list = paste0(here::here(),
                             "/data/test_file_",
                             year,
