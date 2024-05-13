5c9402a61b813bd39678f69f88090efea6614b10
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-05-13 13:56:35 -0700

    Tweak Kelsey's code; save depths as grid26_depth object.

--- data-raw/depth/depth.R
+++ data-raw/depth/depth.R
@@ -4,7 +4,8 @@
 #Author: Kelsey Flynn
 
 #Required packages
-library(pacea)
+# library(pacea)
+load_all()
 library(sf)
 library(terra)
 library(tidyterra)
@@ -22,30 +23,29 @@ library(ggplot2)
 grid26
 
 #grab crs from grid
-crs_new<-paste0(terra::crs(grid26, describe=T)[,2],
-                ":",
-                terra::crs(grid26, describe=T)[,3])
+crs_new <- paste0(terra::crs(grid26, describe=TRUE)[,2],
+                  ":",
+                  terra::crs(grid26, describe=TRUE)[,3])
 
 #read in gebco ncdf as terra spatraster
-bathy<-terra::rast(here("data",
-                        "bathymetry",
-                        "gebco_2023_n58.0_s42.0_w-146.0_e-117.0.nc"))
+bathy <- terra::rast(here::here("data-raw/depth/",
+                              "gebco_2023_n58.0_s42.0_w-146.0_e-117.0.nc"))
 
 
 #template raster to transform bathy data to match pacea grid
-ext2<-st_bbox(grid26)
+ext2 <- st_bbox(grid26)
 
-temp<-terra::rast(resolution=c(500,500), #have to resample to lower res than input data
-                  xmin=ext2[1]-500, #match extent + buffer of pacea grid
-                  ymin=ext2[2]-500,
-                  xmax=ext2[3]+500,
-                  ymax=ext2[4]+500,
-                  crs=crs_new #crs of pacea grid for zonal statistics
-                  )
+temp <- terra::rast(resolution=c(500,500), #have to resample to lower res than input data
+                    xmin=ext2[1]-500, #match extent + buffer of pacea grid
+                    ymin=ext2[2]-500,
+                    xmax=ext2[3]+500,
+                    ymax=ext2[4]+500,
+                    crs=crs_new #crs of pacea grid for zonal statistics
+                    )
 
 
 #reproject to new crs, using bilinear for continuous data
-bathy_proj<-terra::project(bathy, temp, method="bilinear")
+bathy_proj <- terra::project(bathy, temp, method="bilinear")
 bathy_proj
 
 
@@ -65,23 +65,42 @@ grid26 %>%
   geom_sf()
 
 #Zonal statistics
-grid26$mean_depth<-exactextractr::exact_extract(bathy_proj, grid26, "mean")
-grid26$max_depth<-exactextractr::exact_extract(bathy_proj, grid26, "max")
-grid26$min_depth<-exactextractr::exact_extract(bathy_proj, grid26, "min")
+grid26$mean_depth <- exactextractr::exact_extract(bathy_proj, grid26, "mean")
+grid26$max_depth <- exactextractr::exact_extract(bathy_proj, grid26, "max")
+grid26$min_depth <- exactextractr::exact_extract(bathy_proj, grid26, "min")
 
 #lookieloo
 grid26 %>%
-  ggplot(.)+
+  ggplot(.) +
   geom_sf(aes(fill=mean_depth),colour=NA)
 
 #Any average depths above 0 meter elevation?
 grid26 %>%
   mutate(above0=ifelse(mean_depth>0, "Y", "N")) %>%
-ggplot(.)+
+  ggplot(.) +
   geom_sf(aes(fill=above0),colour=NA)
 
 #Any minimum depths above 0 meter elevation?
 grid26 %>%
   mutate(above0_min=ifelse(min_depth>0, "Y", "N")) %>%
-  ggplot(.)+
+  ggplot(.) +
   geom_sf(aes(fill=above0_min),colour=NA)
+
+
+plot(grid26["mean_depth"])     # shows the grid also
+
+# This is nicer for the plot (from above)
+grid26 %>%
+  ggplot(.) +
+  geom_sf(aes(fill=mean_depth),
+          colour=NA)
+
+# Actual values are:
+grid26["mean_depth"]    # As sf object still, mean_depth and geometry
+head(grid26$mean_depth) # Vector of the mean depths, so just showing first 6
+length(grid26$mean_depth) # Vector of the mean depths, so just showing first 6
+
+grid26_depth <- grid26   # As expect changing existing grid26 will mess up
+                              # other code
+usethis::use_data(grid26_depth,
+                  overwrite = TRUE)
