3aaa737f77ecf687f8fa2f71562217185eb81217
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-10-04 11:09:23 -0700

    Created grid_hotssea (not pushed yet in case it changes), #58.

--- data-raw/grids/create-grid-hotssea.R
+++ data-raw/grids/create-grid-hotssea.R
@@ -1,4 +1,6 @@
-# create empty sf grid
+# Create the empty sf grid for hotssea, based on create-grid.R for ROMS
+# outputs. Making this one 1500m to match the hotssea output (though it won't
+# exactly match the curvilinear rotated grid).
 
 library(terra)
 library(sf)
@@ -6,6 +8,7 @@ library(dplyr)
 library(ncdf4)
 
 load_all()
+dir <- here::here()   # Will give pacea/
 
 sf_use_s2(FALSE)
 
@@ -13,35 +16,43 @@ sf_use_s2(FALSE)
 # START - load data to environment
 
 # transform bc_coast
-tbc <- st_transform(bc_coast, crs=crs(bccm_eez_poly))
+#tbc <- st_transform(bc_coast, crs=crs(bccm_eez_poly))
 
 # convert to multilinestring
-tbc.line <- st_cast(tbc, "MULTILINESTRING")
+#tbc.line <- st_cast(tbc, "MULTILINESTRING")
 
 #####
 # create polygons for cropping to roms data
-nc_dat <- nc_open(paste0("data-raw/roms/bcc42_era5glo12r4_mon1993to2019_botTSO.nc"))
+nc_dat <- nc_open(paste0(dir, "/data-raw/hotssea/hotssea_1980to2018_monthly_0to4m_tempsalin_avg.nc"))
 
-# load lon-lat and mask layers from netcdf
-nc_lon <- as.vector(ncvar_get(nc_dat, "lon_rho"))
-nc_lat <- as.vector(ncvar_get(nc_dat, "lat_rho"))
+# data-raw/roms/bcc42_era5glo12r4_mon1993to2019_botTSO.nc")) # Travis had a
+                                        # different .nc file to mask-layer, but
+                                        # grid is presumably the same (though
+                                        # this is bottom).
 
-nc_var <- ncvar_get(nc_dat, "temp")
+# load lon-lat (no mask layers) from netcdf
+nc_lon <- as.vector(ncvar_get(nc_dat, "nav_lon"))   # (French since NEMO)
+nc_lat <- as.vector(ncvar_get(nc_dat, "nav_lat"))
+
+nc_var <- ncvar_get(nc_dat, "votemper")
 nc_varmat <- apply(nc_var, 3, c)
 
 # put sst into dataframe and sf object
-dat <- data.frame(x = nc_lon, y = nc_lat) %>% cbind(nc_varmat) %>%
+dat <- data.frame(x = nc_lon, y = nc_lat) %>%
+  cbind(nc_varmat) %>%
   st_as_sf(coords = c("x", "y"), crs = "EPSG:4326") %>%
   st_transform(crs = "EPSG: 3005")
 
-# create polygon for cropping ROMS data
-roms_cave <- dat %>%
+# create polygon for cropping hotssea data
+hotssea_cave <- dat %>%
   na.omit() %>%
   concaveman::concaveman()
-roms_buff <- dat %>%
+# Skipping buffer:
+hotssea_buff <- dat %>%
   na.omit() %>%
   st_geometry() %>%
-  st_buffer(dist = 5000) %>%
+  st_buffer(dist = 5000) %>%    # TODO don't think we want this, as will end up
+                                # on land
   st_union()
 
 rm(nc_dat, nc_lon, nc_lat, nc_var, nc_varmat, dat)
@@ -49,37 +60,25 @@ rm(nc_dat, nc_lon, nc_lat, nc_var, nc_varmat, dat)
 
 #####
 # make sf grid
-grid2 <- st_make_grid(inshore_poly, cellsize = 2000) %>%
-  st_as_sf()
-grid2 <- grid2[inshore_poly,]
+#grid2 <- st_make_grid(inshore_poly, cellsize = 2000) %>%
+#  st_as_sf()
+# grid2 <- grid2[inshore_poly,]  - don't need this for hottsea
 
-grid6 <- st_make_grid(offshore_poly, cellsize = 6000) %>%
+grid_hotssea <- st_make_grid(hotssea_cave,
+                             cellsize = 1500) %>%
   st_as_sf()
-grid6 <- grid6[offshore_poly,]
 
 # mask 2k grid with 6k grid, then combine grids
-tgrid2 <- grid2[!st_intersects(st_union(grid6), grid2, sparse=F, prepared=T),] %>%
-  rbind(grid2[st_intersects(st_union(grid6), grid2, sparse=F, prepared=T),])
-tgrid26 <- tgrid2 %>% rbind(grid6)
-
-
-##### BC MASK OPTION 1 - Using bc shapefile
-# # index points that dont intersect with bc coast shapefile
-# #  disjoint - do not share space
-# dis2 <- tgrid26[st_disjoint(st_union(tbc), tgrid26, sparse=F, prepared=T),]
-#
-# #  convert bc coast to sf linestring and finding coastline intserections separately - increased processing speed
-# #  using st_intersects is much faster than other predicate functionss
-# sub.t2 <- tgrid26[st_intersects(st_union(tbc), tgrid26, sparse=F, prepared=T),]
-# inter.line <- sub.t2[st_intersects(tbc.line, sub.t2, sparse=F, prepared=T),]
-# grid26 <- rbind(dis2, inter.line)
-
+#tgrid2 <- grid2[!st_intersects(st_union(grid6), grid2, sparse=F, prepared=T),] %>%
+#  rbind(grid2[st_intersects(st_union(grid6), grid2, sparse=F, prepared=T),])
+#tgrid26 <- tgrid2 %>% rbind(grid6)
 
 ##### BC MASK OPTION 2 - Using roms outline
 # use roms_cave
-tgrid26 <- tgrid26[roms_cave,]
+# tgrid26 <- tgrid26[roms_cave,]  # TODO don't think need for hotssea
 
 # use roms_buff to get haida gwaii outline
-grid26 <- tgrid26[roms_buff,]
+# grid26 <- tgrid26[roms_buff,]    # TODO don't think need for hotssea
 
-usethis::use_data(grid26, overwrite = F)
+usethis::use_data(grid_hotssea,
+                  overwrite = TRUE)
