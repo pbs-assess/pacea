c4d6e5b9213a03f424539f3882e0a018299f07bc
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-10-08 14:31:50 -0700

    Hotssea-data-interpolation - copy -andy version into the main one and delete former, copy Greig's to -greig to keep (though think it's all been used).

--- data-raw/hotssea/hotssea-data-interpolation.R
+++ data-raw/hotssea/hotssea-data-interpolation.R
@@ -1,9 +1,20 @@
-# HOTSSea data from Greig Oldford - Salinity and Temperature 1980-2018
-# model depths vary by grid cell, usually only slightly at the surface
-# fields are therefore processed accounting for varying depth level spans.
-# last updated 2024-10-03
+# Currently have to run this; keep doing to get plotting (and 0's) figured out,
+# then work out the saving. Simply doing readRDS on the saved file, but need to
+# incorporate into Travis's anyway.
 
-#library(devtools)
+# Andy starting again from Travis's roms-data-interpolation.R, incorporating Greig's additions as I go
+# along (easier to follow what was commented out by Greig). Copy this over to
+# hottsee-data-interpolation.R when finalised.
+
+# HOTSSea data from Greig Oldford - Salinity and Temperature 1980-2018
+# model depths vary by grid cell, usually only slightly at the surface.
+# Fields are therefore processed accounting for varying depth level spans.
+
+# Run this from the data-raw/hotssea/ directory
+
+# BCCM was ROMs output from Angelina Pena - full bottom and surface variables that Andy requested
+
+library(devtools)
 library(dplyr)
 library(terra)
 library(gstat)
@@ -13,11 +24,11 @@ library(ncdf4)
 library(ggplot2)
 library(concaveman)
 
-
 sf_use_s2(FALSE)  # remove spherical geometry (s2) for sf operations
 
 # load pacea
-#load_all()
+load_all()
+dir <- here::here()   # Will give pacea/
 
 #####
 # START - load data to environment
@@ -28,250 +39,312 @@ tbc <- bc_coast
 # convert to multilinestring
 tbc.line <- st_cast(tbc, "MULTILINESTRING")
 
-# #####
-# # create polygons for cropping to roms data
-# nc_dat <- nc_open(paste0("data-raw/roms/bcc42_era5glo12r4_mon1993to2019_botTSO.nc"))
-# 
-# # load lon-lat and mask layers from netcdf
-# nc_lon <- as.vector(ncvar_get(nc_dat, "lon_rho"))
-# nc_lat <- as.vector(ncvar_get(nc_dat, "lat_rho"))
-# 
-# nc_var <- ncvar_get(nc_dat, "temp")
-# nc_varmat <- apply(nc_var, 3, c)
-# 
-# # put sst into dataframe and sf object
-# dat <- data.frame(x = nc_lon, y = nc_lat) %>% cbind(nc_varmat) %>%
-#   st_as_sf(coords = c("x", "y"), crs = "EPSG:4326") %>%
-#   st_transform(crs = "EPSG: 3005")
-# 
-# # create polygon for cropping ROMS data
-# roms_cave <- dat %>% 
-#   na.omit() %>%
-#   concaveman::concaveman()
-# roms_buff <- dat %>% 
-#   na.omit() %>%
-#   st_geometry() %>% 
-#   st_buffer(dist = 5000) %>%
-#   st_union()
-# 
-# rm(nc_dat, nc_lon, nc_lat, nc_var, nc_varmat, dat)
-# #####
-
 
 #####
 # PARAMETERS
 
 
-
-# LOOP THROUGH DEPTH-INTEGRATED VARIABLES (temp and salin)
+# OPTION 1 FOR LOOPING THROUGH VARIABLES FOR EACH DEPTH
 # loop variables
-ifiles <- list.files("./data-raw/hotssea/", pattern = "tempsalin_avg.nc")
-jvars <- c("votemper", "vosaline")
+ifiles <- list.files(pattern = "tempsalin_avg.nc")
+jvars <- c("votemper", "vosaline")   # French since NEMO
 
 # index table
-vars_fullname <- c("temperature", "salinity")
-vars_units <- c("Temperature (potential; Celcius)",
+vars_fullname <- c("temperature",
+                   "salinity")
+vars_units <- c("Temperature (\u00B0C)",
+  #  "Temperature (potential; \u00B0C)",  TODO Greig had this, need to tweak
+  #   plot_pacea_st() though
                 "Salinity (PSU)")
-jvars_table <- cbind(jvars, vars_fullname, vars_units)
+jvars_table <- cbind(jvars,
+                     vars_fullname,
+                     vars_units)
 
-
+# OPTION 2 FOR LOOPING THROUGH ONLY SURFACE VARIABLES (PRIMARY PRODUCTION)
+# loop variables - won't be needed for hotssea
 
 # function argument
 llnames <- c("x", "y")
 nmax <- 4
 
-# column names
-cnames <- paste(rep(1980:2018, each=12), 1:12, sep="_")
+# column names, adapting Travis's BCCM one. But then adapting Greig's more
+#  automated version. See below as is automatic from the .nc file.
+# cnames <- paste(rep(1980:2018, each=12),
+#                1:12,
+#                sep="_")
 
 # version of data update
-version <- "01"  
+version <- "01"
 
 # processing times output
 proctimes <- vector()
 
 # surface mask layer
-snc_dat <- nc_open("data-raw/hotssea/hotssea_1980to2018_monthly_0to4m_tempsalin_avg.nc")
+snc_dat <- nc_open(paste0(dir, "/data-raw/hotssea/hotssea_1980to2018_monthly_0to4m_tempsalin_avg.nc"))
 snc_lon <- as.vector(ncvar_get(snc_dat, "nav_lon"))
 snc_lat <- as.vector(ncvar_get(snc_dat, "nav_lat"))
 svar <- as.vector(ncvar_get(snc_dat, "votemper", count = c(-1, -1, 1)))
+
+# Adapting Greig's automated suggestion
+snc_time_counter <- ncvar_get(snc_dat, "time_counter")
+snc_time_dates <- as.POSIXct(snc_time_counter, origin = "1900-01-01", tz = "UTC")
+
+# Removes leading zeros for months, so matches Travis's style.
+cnames <-
+  stringr::str_glue("{lubridate::year(snc_time_dates)}_{lubridate::month(snc_time_dates)}") %>%
+  as.vector()
+
 
 sdat <- data.frame(x = snc_lon, y = snc_lat, value = svar) %>%
   st_as_sf(coords = c("x", "y"), crs = "EPSG:4326") %>%
   st_transform(crs = "EPSG:3005")
 
-sroms_cave <- sdat %>% 
+surf_hotssea_cave <- sdat %>%
   na.omit() %>%
   concaveman::concaveman()
-sroms_buff <- sdat %>% 
+  # plots as a rectangle-ish
+
+## mask with coastline, I think this is kind of a fix as we've used surface info
+surf_hotssea_buff <- sdat %>%
   na.omit() %>%
-  st_geometry() %>% 
-  st_buffer(dist = 2000) %>%
+  st_geometry() %>%
+  st_buffer(dist = 1500) %>%
   st_union() %>%
   st_as_sf()
+# Kind of looks the same on the plot, actually that could just be a rescaling
 
 rm(snc_dat, snc_lon, snc_lat, svar, sdat)
 # END parameters
 #####
 
 
+for(i in ifiles[1]){   # ifiles) {  # TODO put back in for all of them
+# i <- ifiles[1]   # for running line by line
+  nc_dat <- nc_open(i)
 
-for(i in ifiles) {
-  nc_dat <- nc_open(paste0("data-raw/hotssea/",i))
-  
   # load lon-lat and mask layers from netcdf
   nc_lon <- as.vector(ncvar_get(nc_dat, "nav_lon"))
   nc_lat <- as.vector(ncvar_get(nc_dat, "nav_lat"))
 
   # depth from file name
   # hotssea_1980to2018_monthly_0to4m_tempsalin_avg.nc
-  if(substr(i, 33, 35) %in% c("bot", "sur")){
-    ti <- substr(i, 33, 35)
-    if(ti == "bot") {ti <- "bottom"} else {ti <- "surface"}
-  } else {
-    ti <- strsplit(substr(i, 33, nchar(i)), "_")[[1]][1]
-  }
-  
+  # This was Travis's, will have to tailor, the 33,35 looks a bit risky TODO
+  ## if(substr(i, 33, 35) %in% c("bot", "sur")){
+  ##   ti <- substr(i, 33, 35)
+  ##   if(ti == "bot") {ti <- "bottom"} else {ti <- "surface"}
+  ## } else {
+  ##   ti <- strsplit(substr(i, 33, nchar(i)), "_")[[1]][1]
+  ## }
+
   # temp hardcode till fix above - GO
-  ti = "0to4m"
-  
-  for(j in jvars) {
-    
+  ti = "0to10m"
+
+  for(j in c("votemper")){   # jvars) {   # TODO put back in for loop
+    # j <- "votemper"  # for running line-by-line
     start <- Sys.time()
-    
+
     nc_var <- ncvar_get(nc_dat, j)
     nc_varmat <- apply(nc_var, 3, c)
-    
-    # put into dataframe and sf object
-    dat <- data.frame(x = nc_lon, y = nc_lat) %>% cbind(nc_varmat)
-    dat_sf <- st_as_sf(dat, coords = c("x", "y"), crs = "EPSG:4326")
-    tdat_sf <- st_transform(dat_sf, crs = "EPSG: 3005")
-    
-    # create polygon for cropping hotssea data
-    hotssea_cave <- tdat_sf %>% 
+
+    # put sst into dataframe and sf object
+    dat <- data.frame(x = nc_lon,
+                      y = nc_lat) %>%
+      cbind(nc_varmat)
+    dat_sf <- st_as_sf(dat,
+                       coords = c("x", "y"),
+                       crs = "EPSG:4326")    # Okay for Greig's
+    tdat_sf <- st_transform(dat_sf, crs = "EPSG: 3005")                 # BC Albers
+
+ # Gives this, where 39468 = 132 cells x 299 cells I think no need to crop
+ #> tdat_sf
+ # Simple feature collection with 39468 features and 468 fields
+ # Geometry type: POINT
+ # Dimension:     XY
+ # Bounding box:  xmin: 972441.8 ymin: 209502.4 xmax: 1351568 ymax: 676720.4
+ # Projected CRS: NAD83 / BC Albers
+# First 10 features:
+#   1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28
+#1  0 0 0 0 0 0 0 0 0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
+#2  0 0 0 0 0 0 0 0 0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
+#3  0 0 0 0 0 0 0 0 0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
+
+# plot(tdat_sf) gives 9 plots, and warning: plotting the first 9 out of 468
+# attributes; use max.plot = 468 to plot all. But maps of first 9 look different
+# to each other. Must be differences further down. Attributes are columns
+    # (fields) I think corresponding to months, since 1980 to 2018 monthly, and
+    # (2018-1980)*12 = 456.
+
+    # 0's may be the masking?
+
+# plot(dat_sf) and plot(tdat_sf) look pretty similar, slightly rotated
+# (different projection)
+
+# Andy tried manually to crop with this, but should jsut have used same approach
+# of cave and buffer, so commenting these out:
+## stopifnot(ncol(tdat_sf) == 469)    # if fails then change 468 below, note we
+##                                    # remove geometry column
+
+## tdat_sf_tib <- tibble::as_tibble(tdat_sf) %>%
+##   dplyr::select(-c("geometry")) %>%
+##   dplyr::rowwise() %>%
+##   dplyr::mutate(min = min(c_across(`1`:`468`)),
+##                 max = max(c_across(`1`:`468`)))  # Takes a few minutes
+
+## # This is stuff Andy added that I think messes things up and is why it doesn't plot
+## only_zeros_indices <- which(tdat_sf_tib$min == 0 & tdat_sf_tib$max == 0)  # length(only_zeros_indices) is 30086
+## tdat_sf_cropped <- tdat_sf[-only_zeros_indices, ]
+## tdat_sf_cropped
+## dim(tdat_sf_cropped)
+## plot(tdat_sf_cropped)   # works
+
+# This was for ROMS, so adapting now:
+# create polygon for cropping ROMS data
+# The s Travis had is for surface, I hadn't noticed that. So putting surf_ in
+# ones above. Then here inside the loop
+# they're done for each object (because deep ones won't have the same coverage).
+# Some looks lke overkill but I think the conversions were needed to get to the
+# same format.
+    hotssea_cave <- tdat_sf %>%
       na.omit() %>%
       concaveman::concaveman()
-    hotssea_buff <- tdat_sf %>% 
+
+    hotssea_buff <- tdat_sf %>%
       na.omit() %>%
-      st_geometry() %>% 
-      st_buffer(dist = 2000) %>%
+      st_geometry() %>%
+      st_buffer(dist = 1500) %>%
       st_union() %>%
       st_as_sf()
-    
-    print("done")
-    
-    # Ended here - GO 2024-10-02
-    
-    
-    # # interpolate data
-    # # 2 km res
-    # output2 <- point2rast(data = tdat_sf, spatobj = inshore_poly, loc = llnames, cellsize = 2000, nnmax = nmax, as = "SpatRast")
-    # # 6 km res
-    # output6 <- point2rast(data = tdat_sf, spatobj = offshore_poly, loc = llnames, cellsize = 6000, nnmax = nmax, as = "SpatRast")
-    # 
-    # 
-    # # crop out grid cells with polygon masks
-    # t2_sf2 <- output2 %>% 
-    #   mask(bccm_eez_poly) %>% 
-    #   mask(inshore_poly) %>%
-    #   stars::st_as_stars() %>%  ## check here for converting to points (not raster)
-    #   st_as_sf()
-    # t2_sf6 <- output6 %>%
-    #   mask(bccm_eez_poly) %>%
-    #   mask(offshore_poly) %>%
-    #   stars::st_as_stars() %>%
-    #   st_as_sf()
-    # 
-    # # mask 2k grid with 6k grid, then combine grids
-    # t2_sf26a <- t2_sf2[!st_intersects(st_union(t2_sf6), t2_sf2, sparse=FALSE, prepared=TRUE),] %>%
-    #   rbind(t2_sf2[st_intersects(st_union(t2_sf6), t2_sf2, sparse=FALSE, prepared=TRUE),]) %>% 
-    #   rbind(t2_sf6)
-    # 
-    # 
-    # ##### BC MASK OPTION 1 - Using bc shapefile
-    # # index points that dont intersect with bc coast shapefile
-    # #  disjoint - do not share space
-    # # dis2 <- t2_sf26[st_disjoint(st_union(tbc), t2_sf26, sparse=FALSE, prepared=TRUE),]
-    # # 
-    # # #  convert bc coast to sf linestring and finding coastline intersections separately - increased processing speed
-    # # #  using st_intersects is much faster than other predicate functions
-    # # sub.t2 <- t2_sf26[st_intersects(st_union(tbc), t2_sf26, sparse=FALSE, prepared=TRUE),]
-    # # inter.line <- sub.t2[st_intersects(tbc.line, sub.t2, sparse=FALSE, prepared=TRUE),]
-    # # t2_sf26 <- rbind(dis2, inter.line)
-    # 
-    # ##### BC MASK OPTION 2 - Using roms outline 
-    # # 1. use roms_cave
-    # t2_sf26b <- t2_sf26a[roms_cave,]
-    # 
-    # # 2. use roms_buff to get haida gwaii outline and shore
-    # t2_sf26b <- t2_sf26b[roms_buff,]
-    # 
-    # # 3. use default surface roms_cave
-    # t2_sf26b <- t2_sf26b[sroms_cave,]
-    # 
-    # # 4. use default surface roms_buff
-    # t2_sf26 <- t2_sf26b[sroms_buff,]
-    # 
-    # # data should have 41,288 grid cells
-    # # if(nrow(t2_sf26) != 41288){
-    # #   out.msg <- paste0(as.symbol(t2_sf26), " nrows = ", nrow(get(objname)), 
-    # #                     ". nrows not equal to 13,377,312...wrangle to long format (or somethinig else) failed.")
-    # #   stop(out.msg)
-    # # }
-    # 
-    # # assign column names as year_month
-    # names(t2_sf26)[1:(ncol(t2_sf26) - 1)] <- cnames 
-    # 
-    # # covert to long format data - Don't do long format as it is too big
-    # # t3_sf26 <- t2_sf26 %>%
-    # #   tidyr::pivot_longer(cols = !last_col(), cols_vary = "slowest", names_to = "date", values_to = "value")  %>%
-    # #   mutate(year = substr(date, 1, 4),
-    # #          month = substr(date, 6, 7)) %>%
-    # #   dplyr::select(-date) %>%
-    # #   relocate(value, .after = last_col()) %>%
-    # #   relocate(geometry, .after = last_col()) 
-    # 
-    # # round to 6 decimal places to reduce file size 
-    # # t3_sf26[, "value"] <- t3_sf26$value %>% # for long format
-    # #   round(digits = 6)
-    # t3_sf26 <- t2_sf26 %>%
-    #   st_drop_geometry() %>%
-    #   round(digits = 6) %>%
-    #   st_as_sf(geometry = st_geometry(t2_sf26))
-    # 
-    # # assign pacea class 
-    # class(t3_sf26) <- c("pacea_st", "sf", "tbl_df", "tbl", "data.frame")
-    # 
-    # # assign units attribute
-    # attr(t3_sf26, "units") <- jvars_table[which(jvars_table[, 1] == j), 3]
-    # 
-    # # name file and write data
-    # tj <- jvars_table[which(jvars_table[, 1] == j), 2]
-    # if(ti == "zInt"){
-    #   objname <- paste("bccm", tj, sep = "_")
-    # } else {
-    #   objname <- paste("bccm", ti, tj, sep = "_")
+
+    output2 <- point2rast(data = tdat_sf,
+                          spatobj = hotssea_poly,
+                          loc = llnames,
+                          cellsize = 1500,       # Want 1500 not 2000
+                          nnmax = nmax,
+                          as = "SpatRast")
+
+    # This is a "SpatRaster" object, doesn't plot well
+    #plot(output2) # with roms_buff gave fancy artwork. Looks wrong but could be the
+    # plotting as it's a SpatRaster.
+    # plot(output2)  - now gives something sensible, using my new hotssea_poly
+
+    # 6 km res, not needed here
+    #    output6 <- point2rast(data = tdat_sf, spatobj = offshore_poly, loc = llnames, cellsize = 6000, nnmax = nmax, as = "SpatRast")
+
+# crop out grid cells with polygon masks
+    t2_sf <- output2 %>%
+      terra::mask(hotssea_poly) %>%  #      mask(inshore_poly) %>%
+      stars::st_as_stars() %>%  ## check here for converting to points (not raster)
+      st_as_sf()
+
+    # mask gives a warning but I think still uses terra::mask:
+    # Warning message:
+    #  In findGeneric(f, envir) :
+    # 'mask' is a formal generic function; S3 methods will not likely be found
+
+    # Not needed presumably:
+    #    t2_sf6 <- output6 %>%
+    #      mask(bccm_eez_poly) %>%
+    #      mask(offshore_poly) %>%
+    #      stars::st_as_stars() %>%
+    #     st_as_sf()
+
+    # plot(t2_sf2)  nope, each does look the same though
+
+    # TODO not sure if this step adds anything in beyond just combining the grids
+    # mask 2k grid with 6k grid, then combine grids
+#    t2_sf26a <- t2_sf2[!st_intersects(st_union(t2_sf6), t2_sf2, sparse=FALSE, prepared=TRUE),] %>%
+#      rbind(t2_sf2[st_intersects(st_union(t2_sf6), t2_sf2, sparse=FALSE, prepared=TRUE),]) %>%
+#      rbind(t2_sf6)
+
+    ##### BC MASK OPTION 2 - Using roms outline
+    # 1. use roms_cave
+    t2_sfb <- t2_sf[hotssea_cave, ]
+
+    # 2. use roms_buff to get haida gwaii outline and shore - do, as likely
+    # needed for elsewhere:
+    t2_sfb <- t2_sfb[hotssea_buff,]
+
+    # 3. use default surface roms_cave
+    t2_sfb <- t2_sfb[surf_hotssea_cave,]
+
+    # 4. use default surface roms_buff
+    t2_sf <- t2_sfb[surf_hotssea_buff,]    # Carefull, going back to t2_sf
+    # Simple feature collection with 34515 features and 468 fields
+    # Geometry type: POLYGON
+
+    # BCCM:
+    # data should have 41,288 grid cells
+    # if(nrow(t2_sf26) != 41288){
+    #   out.msg <- paste0(as.symbol(t2_sf26), " nrows = ", nrow(get(objname)),
+    #                     ". nrows not equal to 13,377,312...wrangle to long format (or somethinig else) failed.")
+    #   stop(out.msg)
     # }
-    # filename <- paste0("../pacea-data/data/",objname, "_", version, ".rds")
-    # #filename <- paste0("../pacea-data/data/",objname, ".rds")
-    # assign(objname, t3_sf26)
-    # 
-    # do.call("save", list(as.name(objname), file = filename, compress = "xz"))
-    # 
-    # end <- Sys.time()
-    # jtime <- end-start
-    # print(jtime)
-    # names(jtime) <- paste(ti, tj, sep="_")
-    # proctimes <- c(proctimes, jtime)
-    # 
-    # # remove files 
-    # rm(dat, dat_sf, tdat_sf, roms_cave, roms_buff,
-    #    output2, output6, t2_sf2, t2_sf6, t2_sf26,
-    #    t2_sf26a, t2_sf26b, t3_sf26, nc_var, nc_varmat)
-    # rm(list = objname)
-    # gc()
+
+    # assign column names as year_month
+    names(t2_sf)[1:(ncol(t2_sf) - 1)] <- cnames
+
+    # round to 6 decimal places to reduce file size
+    t3_sf <- t2_sf %>%
+      st_drop_geometry() %>%
+      round(digits = 6) %>%
+      st_as_sf(geometry = st_geometry(t2_sf))
+
+
+    # assign pacea class
+
+    # Travis had this, but I think safer to do line after since it isn't a tibble
+    class(t3_sf) <- c("pacea_st",
+                      "sf",
+                      "tbl_df",
+                      "tbl",
+                      "data.frame")
+
+# This works here while still just an sf object:
+# plot(tdat_sf_cropped_2, cex = 0.6, pch = 16)
+
+# class(tdat_sf_cropped_2) <- c("pacea_st",
+#                              class(tdat_sf_cropped_2))
+
+
+    # assign units attribute
+    attr(t3_sf, "units") <- jvars_table[which(jvars_table[, 1] == j), 3]
+    attr(t3_sf, "restrict_plotting_range") <- TRUE      # To then use to automatically restrict
+                                        # the plotting
+    attr(t3_sf, "salinity_unit") <- "PSU"      # To automate the axes labels
+
+# doesn't work though, using plot.pacea_st(), even after doing th cropping
+# etc. above.
+# AHA think because variable names are slightly different
+# plot(tdat_sf_cropped_2, cex = 0.6, pch = 16)
+
+
+    # name file and write data
+    tj <- jvars_table[which(jvars_table[, 1] == j), 2]
+    if(ti == "zInt"){
+      objname <- paste("hotssea", tj, sep = "_")
+    } else {
+      objname <- paste("hotssea", ti, tj, sep = "_")    # TODO add in mean
+    }
+#    filename <- paste0("../pacea-data/data/",objname, "_", version, ".rds")
+#    TODO, for now dumping in same folder
+    filename <- paste0(objname, "_", version, ".rds")
+    #filename <- paste0("../pacea-data/data/",objname, ".rds")
+    assign(objname, t3_sf)
+
+    do.call("save", list(as.name(objname), file = filename, compress = "xz"))
+
+    end <- Sys.time()
+    jtime <- end-start
+    print(jtime)
+    names(jtime) <- paste(ti, tj, sep="_")
+    proctimes <- c(proctimes, jtime)
+
+    # remove files
+    # TODO update this:
+    rm(dat, dat_sf, tdat_sf, roms_cave, roms_buff,
+       output2, output6, t2_sf2, t2_sf6, t2_sf26,
+       t2_sf26a, t2_sf26b, t3_sf26, nc_var, nc_varmat)
+    # rm(list = objname)   # TODO add back in when have saved and reloaded .rds
+    gc()
   }
 }
-
-
-
-
