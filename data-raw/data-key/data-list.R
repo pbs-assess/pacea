e8c68cab51ad2e469aae2157fe4a43fcd95e9285
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-11-03 12:41:06 -0800

    Create bccm_data_full data object.

--- data-raw/data-key/data-list.R
+++ data-raw/data-key/data-list.R
@@ -8,6 +8,18 @@ bccm_data <- read.csv(paste0(here::here(),
 use_data(bccm_data,
          overwrite = TRUE)
 
+bccm_data_full <- bccm_data
+for(i in 1:nrow(bccm_data_full)){
+  bccm_data_full[i, "data_name"] <- paste0(bccm_data_full[i, "data_name"],
+                                           "_full")
+}
+
+use_data(bccm_data_full,
+         overwrite = TRUE)
+
+
+# Same names are used for bccm_full, just with _full appended. So no need to
+#  save another data object here.
 
 hotssea_data <- read.csv(paste0(here::here(),
                                 "/data-raw/data-key/hotssea_data_list.csv"),
