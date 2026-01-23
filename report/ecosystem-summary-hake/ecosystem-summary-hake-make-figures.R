d7729eba70576c97f01106fab86c40f3e1031fca
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2026-01-23 14:36:06 -0800

    Note to self about about hake ecosystem summary path.

--- report/ecosystem-summary-hake/ecosystem-summary-hake-make-figures.R
+++ report/ecosystem-summary-hake/ecosystem-summary-hake-make-figures.R
@@ -7,7 +7,7 @@ source("ecosystem-summary-hake-glorys.R")
 # Specific to Andy's laptop:
 hake_assessment_fig_dir <- paste0(getwd(), "/")
 # hake_assessment_fig_dir <- paste0(here::here(),
-# TODO                                  "/../hake-assessment-2024/doc/image-files/") # 2024 is generic directory for 2024 onwards
+# This was used for 2025 and 2026:                                  "/../hake-assessment-2024/doc/image-files/") # 2024 is generic directory for 2024 onwards
 
 png(paste0(hake_assessment_fig_dir,
            "ecosystem_summary_from_pacea.png"),
