d7c30b257c2ac23f44b05f7a70e169fa90d45d86
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2026-09-14 16:12:25 -0700

    Simplify create_index.pacea_recruitment_herring() to then call the more general create_index.pacea_recruitment().

--- R/create-index-pacea-recruitment-herring.R
+++ R/create-index-pacea-recruitment-herring.R
@@ -4,10 +4,11 @@ create_index.pacea_recruitment_herring <- function(data,
                                                    years = NULL,
                                                    herring_region = "WCVI",
                                                    index_label = NULL,
-                                                   index_name = NULL){
-  if(is.null(years)){
-    years <- min(data$year):max(data$year)
-  }
+                                                   index_name = NULL,
+                                                   index_statistic = "median"){
+  result <- filter(data,
+                   region == herring_region) %>%
+    select(-c("region"))
 
   if(is.null(index_label)){
     index_label <- paste0("Herring ",
@@ -15,26 +16,15 @@ create_index.pacea_recruitment_herring <- function(data,
                           " age-2 recruitment")
   }
 
-  res <- dplyr::filter(data,
-                       year %in% years,
-                       region == herring_region)
-
   if(is.null(index_name)){
     index_name <- paste0(deparse(substitute(data)),
                          "_",
                          tolower(herring_region))
   }
 
-  res <- res %>%
-    dplyr::mutate(value = standardise(median),
-                  index = index_name,
-                  index_label = index_label) %>%
-    dplyr::select(index,
-                  year,
-                  value,
-                  index_label)
-
-  class(res)[1] <- "pacea_standardised_index"
-
-  res
+  create_index.pacea_recruitment(data = result,
+                                 years = years,
+                                 index_label = index_label,
+                                 index_name = index_name,
+                                 index_statistic = index_statistic)
 }
