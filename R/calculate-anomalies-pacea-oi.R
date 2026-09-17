8f1d75005ecdc6da8fd5725c4a053c3c6d1e0841
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2026-09-17 16:38:16 -0700

    Add new files for previous commit.

---
+++ R/calculate-anomalies-pacea-oi.R
@@ -0,0 +1,192 @@
+#' TODO all the help Calculate climatology and anomalies for buoy SST data.
+#'
+#' Function for calculating climatology and subsequent anomalies of a `buoy_sst` pacea data object.
+#'
+#' @param data `buoy_sst` pacea data object
+#' @param climatology_years climatology period years
+#' @param climatology_time time units ("month" or "week") to summarize
+#' climatologies and anomalies, but only "month" is currently available (there
+#' is some functionality to use "week" also -- contact Andy if this would be useful).
+#' @param time_period_return vector of value(s) for the specific time units to estimate climatologies (e.g. '4' for week 4 or April). Set to equal 'all' for all time units.
+#' @param years_return vector of value(s) to return the years of
+#' interest. Defaults to all years in input data
+#' @param min_days_per_month minimum number of daily SST values required in a
+#' time period (month or week) for that period to be included in climatology
+#' calculation and anomalies calculation. Defaults to 15 days per month.
+#' @param max_consecutive_missing_days maximum number of consecutive days allowed
+#' to be missing (NA) within a time period (month or week). If a time period has
+#' more than this many consecutive NAs, it is excluded from climatology and anomalies
+#' calculation. Defaults to 6 days.
+#'
+#' @importFrom dplyr mutate select filter group_by summarise ungroup left_join join_by rename relocate
+#' @importFrom sf st_drop_geometry st_as_sf
+#' @importFrom tidyr pivot_longer
+#' @importFrom lubridate year
+#' @importFrom stats sd
+#'
+#' @return pacea_buoy_anomalies_list object (also a list) of climatology of data and anomalies, of class
+#' `pacea_buoy_anomalies_list`. Note that `climatology_years` will be the
+#' prescribed years, but these may not be available for all buoys.
+#' @export
+#'
+#' @author Andrew Edwards and Travis Tai
+#' @examples
+#' \dontrun{
+#' area_126 <- tibble(
+#'   lat = c(-127.1506, -128.2331, -129.3492, -127.9167, -127.1847, -126.8200,
+#' -127.1506),
+#'   lon = c(49.85766, 49.00000, 48.99991, 50.11915, 50.40183, 50.24466, 49.85766))
+#' res <- calculate_anomalies(oisst_month, area = area_126)
+#' }
+calculate_anomalies.pacea_oi <- function(data,                # TODO only for
+                                         # monthly oisst_month
+                                         climatology_years = 1991:2020,
+                                         climatology_time = "month",
+                                         time_period_return = "all",
+                                         years_return = NULL,
+                                         area = NULL){  # characters string of a
+  # named area (only pfma_area_126 at the moment), or data.frame, tibble, or
+  # matrix with columns as latitude and longitude of points around the region,
+  # with the final point equalling the first one (so defining a closed
+  # polygon). If NULL then the whole region of data.
+
+  if(is.matrix(area) | is.data.frame(area)){
+    area_as_list_of_matrix <- list(area)
+  }
+
+  if(is.character(area)){
+    # TODO move to a function, need data objects at some point, maybe as sf
+    if(area == "pfma_area_126"){
+      area_as_list_of_matrix <-
+        list(matrix(c(-127.1506, -128.2331, -129.3492, -127.9167, -127.1847,
+                      -126.8200, -127.1506,
+                      49.85766, 49.00000, 48.99991, 50.11915, 50.40183,
+                      50.24466, 49.85766),
+                    ncol = 2))    # From Travis's OISST vignette
+    } else {
+      stop("`area` not accepted yet as a character string")
+    }
+  }
+
+
+
+  # Reduce data to the area specified
+  if(is.null(area)){
+    data_for_area <- data
+  } else {
+    # Create polygon object with lat-lon WGS 84 projection (4326)
+    area_polygon <- sf::st_sfc(sf::st_polygon(area_as_list_of_matrix),
+                               crs = 4326) %>%
+      sf::st_as_sf()
+
+    data_for_area <- data[area_polygon, ]
+  }
+
+
+
+  # example area:
+  #  list(matrix(c(-127.1506, -128.2331, -129.3492, -127.9167, -127.1847, -126.8200, -127.1506,
+  #                    49.85766, 49.00000, 48.99991, 50.11915, 50.40183, 50.24466, 49.85766),
+  #                  ncol = 2))
+
+  stopifnot("'climatology_time' must currently have a value of 'month'; if you want 'week' then email Andy or make an Issue, as some of the code will need updating, and we did not think this was the most important thing to work on"
+            = climatology_time %in% c("month"))
+  # Some of the code and plotting code will need thinking about if we want
+  # "week" to work.
+
+  # climatology_time is irrelevant if class != pacea_buoy    TODO tidyup
+  # Stick with how it comes:
+  # if("pacea_st" %in% class(data)) climatology_time <- "month"   # BCCM
+  if("month" %in% colnames(data)) climatology_time <- "month"
+  # if("week" %in% colnames(data)) climatology_time <- "week"
+
+
+  # index values for time_period_return to subset from data
+  if(time_period_return[1] == "all"){
+    if(climatology_time == "month") time_period_return <- 1:12
+    if(climatology_time == "week") time_period_return <- 1:53
+  }
+
+  if(climatology_time == "month") {
+    m_ind <- month_match(time_period_return)
+    time_period_return <- m_ind
+  }
+
+  if(is.null(years_return)){
+    years_return <- 1800:2100
+  }
+
+  # From calc_clim:
+  climatology <- data_for_area %>%
+    filter(year %in% climatology_years,
+           month %in% time_period_return) %>%
+    group_by(month,
+             geometry) %>%
+    summarise(clim_value = mean(sst,
+                                na.rm = TRUE),
+              clim_sd = sd(sst,
+                           na.rm = TRUE),
+              clim_n = sum(!is.na(sst_n))) %>%
+    ungroup() %>%
+    relocate(geometry,
+             .after = last_col())
+
+  class(climatology) <- c("pacea_oiclim",
+                          "sf",
+                          "tbl_df",
+                          "tbl",
+                          "data.frame")
+  attr(climatology, "units") <- "Temperature (\u00B0C)"
+
+
+  # Anomalies
+  coords <- st_coordinates(data_for_area)   # take out to save time, then put back in later
+
+  dat_nogeometry <- data_for_area %>%
+    select(year,
+           month,
+           sst,
+           start_date,
+           end_date,
+           geometry) %>%
+    mutate(x = coords[, 1],
+           y = coords[, 2]) %>%
+    st_drop_geometry()
+
+  clim_dat <- dat_nogeometry %>%         # Recalculate as same format
+    filter(year %in% years_return) %>%
+    group_by(month,
+             x,
+             y) %>%
+    summarise(clim_value = mean(sst,
+                                na.rm = TRUE)) %>%
+    ungroup()
+
+  anomalies <- dat_nogeometry %>%
+    filter(year %in% years_return,
+           month %in% time_period_return) %>%
+    left_join(clim_dat,
+              by = join_by(month == month,
+                           x == x,
+                           y == y)) %>%
+    mutate(anom = sst - clim_value) %>%
+    st_as_sf(coords = c("x", "y"),
+             crs = st_crs(data_for_area))
+
+  class(anomalies) <- c("pacea_oianom",
+                        "sf",
+                        "tbl_df",
+                        "tbl",
+                        "data.frame")
+  attr(anomalies, "units") <- "Temperature (\u00B0C) anomaly"
+
+  res <- list(climatology = climatology,
+              anomalies = anomalies,
+              climatology_years = climatology_years,
+              climatology_time = climatology_time)
+
+  class(res) <- c("pacea_oisst_anomalies_list",
+                  "list")
+  return(res)
+}
+
---
+++ R/plot-pacea-oisst-anomalies-list.R
@@ -0,0 +1,62 @@
+##' Plot anomalies of the OISST sea-surface temperature values
+##'
+##' Does a spatial plot of the anomalies.  TODO
+##'
+##' @param pacea_oisst_anomalies_list object of class `pacea_oisst_anomalies_list`
+##' obtained from running `caclulate_anomalies()` on oisst values.
+##' @param stn_id_to_plot character vector of station IDs (`stn_id` values OR
+##' `name` values ) from the `buoy_sst` data
+##' object. If `NULL` (default), anomalies for all buoys (in
+##' `pacea_buoy_anomalies_list_object`) are plotted. If specified, only the
+##' buoys in this vector are plotted. If `length(stn_id_to_plot == 1)` then anomalies
+##' for each month are shown, with January at the top and December at the
+##' bottom.
+##' @param months numeric vector of months (1-12) to include. If not specified
+##' then defaults to 4
+##' (April), except when only one `stn_id_to_plot` when all months are plotted (unless specified). Can have more than one month, e.g. 6:9. For a 'winter' average,
+##' say Nov-Mar, specify the months as `c(11, 12, 1, 2, 3)`. The winter average
+##' anomaly will be calculated and named for the year in which January
+##' falls. `months` must be consecutive (except for the overwintering `12, 1` as
+##' in the above example).
+##' @param main title for the plot, if `NULL` then created automatically,
+##' including detailing the months selected. May need to manually specify `main`
+##' if many non-consecutive months are chosen (which seems unlikely).
+##' @param xlab x-axis label
+##' @param ylab y-axis label
+##' @param use_stn_id_name logical, whether to use the name of the buoy
+##' (e.g. `Middle NOMAD`) or the `stn_id` (e.g. C46004). See `buoy_metadata` for
+##' the names.
+##' @param sst_plot character, one of:
+##'   * `"anomalies"` (default) - plots SST anomalies
+##'   * `"mean"` - plots mean SST values for each month
+##'   * `"count"` - plots count of daily SST values used to calculate monthly mean
+##' @param count_breaks numeric vector of break points for the count plot colour scale.
+##' Only used when `sst_plot = "count"`. Default of `NULL` actually defaults to
+##' `c(0, 10, 15, 20, 31) * length(months)` if `months` is specified, else it is
+##' `c(0, 10, 15, 20, 31)`.
+##' @param return_results logical, if `FALSE` (default) returns the plot object only.
+##' If `TRUE`, prints the plot and returns a list with both `plot` and `results`.
+##' @param require_requested_months numeric, number of months that must be available
+##' out of the requested `months` to compute an average anomaly. If `NULL` (default),
+##' defaults to `length(months)`. Only used when `length(months) > 1` and
+##' `length(stn_id_to_plot) > 1`.
+##' @param scale_limits numeric vector of length 2 specifying the limits for the colour scale.
+##' If `NULL` (default), limits are calculated automatically based on the data. Only used
+##' for non-count plots. If any plotted values are outside of `scale_limits`
+##' then they are coloured grey.
+##' @return a ggplot object (when `return_results = FALSE`) or a list with `plot` and `results`
+##' (when `return_results = TRUE`)
+##' @export
+##' @author Andrew Edwards and Andrea Hilborn
+##' @examples
+##' \dontrun{
+##' # See the vignette for explanations and use of the options.
+##' all_buoys_anomalies <- calculate_anomalies(buoy_sst,
+##'                                            climatology_time = "month")
+##' plot(all_buoys_anomalies)
+##' }
+plot.pacea_oisst_anomalies_list <- function(pacea_oisst_anomalies_list,
+                                            ...){
+  plot.pacea_oianom(pacea_oisst_anomalies_list$anomalies,
+                    ...)
+}
